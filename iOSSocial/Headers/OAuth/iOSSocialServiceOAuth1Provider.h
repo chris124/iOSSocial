21cebb95591487ea8798023d0ec8d137b3ac5efe
Christopher White <chris@madraces.com>
2011-09-30 00:35:14 -0400

    cleanup

--- iOSSocial/Headers/OAuth/iOSSocialServiceOAuth1Provider.h
+++ iOSSocial/Headers/OAuth/iOSSocialServiceOAuth1Provider.h
@@ -10,12 +10,29 @@
 #import <UIKit/UIKit.h>
 #import "iOSSocialOAuth.h"
 
+@class GTMOAuthAuthenticationWithAdditions;
 @interface iOSSocialServiceOAuth1Provider : NSObject
 
 - (id)initWithDictionary:(NSDictionary*)dictionary;
 
+- (GTMOAuthAuthenticationWithAdditions*)checkAuthenticationForKeychainItemName:(NSString*)theKeychainItemName;
+
 - (void)assignOAuthParams:(NSDictionary*)params;
 
-- (void)logout;
+- (void)authorizeFromViewController:(UIViewController*)vc 
+                            forAuth:(GTMOAuthAuthenticationWithAdditions*)theAuth 
+                andKeychainItemName:(NSString*)theKeychainItemName 
+                    andCookieDomain:(NSString*)cookieDomain
+              withCompletionHandler:(AuthorizationHandler)completionHandler;
+
+- (void)logout:(GTMOAuthAuthenticationWithAdditions*)theAuth forKeychainItemName:(NSString*)theKeychainItemName;
+
+- (NSString*)apiKey;
+
+- (NSString*)apiSecret;
+
+- (NSString*)authorizationHeaderForRequest:(NSURLRequest *)request withAuth:(GTMOAuthAuthenticationWithAdditions*)auth;
+
++ (id)JSONFromData:(NSData*)data;
 
 @end
