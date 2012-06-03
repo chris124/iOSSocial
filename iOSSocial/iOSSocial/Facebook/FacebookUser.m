d0dde05c04e5a10bd1929879002ec524fcc1a49c
Christopher White <chris@madraces.com>
2012-06-02 20:53:45 -0400

    fix warnings by putting in stubs in facebook user for fbsessiondelegate methods

--- iOSSocial/iOSSocial/Facebook/FacebookUser.m
+++ iOSSocial/iOSSocial/Facebook/FacebookUser.m
@@ -227,4 +227,55 @@ typedef enum _FBRequestType {
     NSLog(@"didLoadRawResponse");
 }
 
+#pragma FBSessionDelegate
+
+/**
+ * Called when the user successfully logged in.
+ */
+- (void)fbDidLogin
+{
+    
+}
+
+/**
+ * Called when the user dismissed the dialog without logging in.
+ */
+- (void)fbDidNotLogin:(BOOL)cancelled
+{
+    
+}
+
+/**
+ * Called after the access token was extended. If your application has any
+ * references to the previous access token (for example, if your application
+ * stores the previous access token in persistent storage), your application
+ * should overwrite the old access token with the new one in this method.
+ * See extendAccessToken for more details.
+ */
+- (void)fbDidExtendToken:(NSString*)accessToken
+               expiresAt:(NSDate*)expiresAt
+{
+    
+}
+
+/**
+ * Called when the user logged out.
+ */
+- (void)fbDidLogout
+{
+    
+}
+
+/**
+ * Called when the current session has expired. This might happen when:
+ *  - the access token expired
+ *  - the app has been disabled
+ *  - the user revoked the app's permissions
+ *  - the user changed his or her password
+ */
+- (void)fbSessionInvalidated
+{
+    
+}
+
 @end
